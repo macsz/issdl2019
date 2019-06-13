4cbec973ba85979f0037f28dc5f322b0db3820ed
Maciej Szankin <maciej@szankin.pl>
2019-06-13 16:57:13 -0700

    Download tensorflow_datasets has been extracted to base image

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ ! -d tensorflow_datasets ]];
+then
+    wget -c http://ml.eti.pg.gda.pl/files/tensorflow_datasets.tar.gz
+    tar -xvf tensorflow_datasets.tar.gz
+    rm -f tensorflow_datasets.tar.gz
+fi
+
 docker build --build-arg all_proxy=${all_proxy} \
              --build-arg ftp_proxy=${ftp_proxy} \
              --build-arg http_proxy=${http_proxy} \
