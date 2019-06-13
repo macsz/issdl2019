e9b039cbe9b7eb506ea6d78141c383d7dfb3be80
Maciej Szankin <maciej@szankin.pl>
2019-06-13 13:07:12 -0700

    Removed proxy configuration from base_py3

--- docker/base_py3/build.sh
+++ docker/base_py3/build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --build-arg all_proxy=${all_proxy} \
-             --build-arg ftp_proxy=${ftp_proxy} \
-             --build-arg http_proxy=${http_proxy} \
-             --build-arg https_proxy=${https_proxy} \
-             --build-arg no_proxy=${no_proxy} \
-             --build-arg socks_proxy=${socks_proxy} \
-             -t macsz/issdl2019/base_py3 \
+docker build -t macsz/issdl2019/base_py3 \
             .
